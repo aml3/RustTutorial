12bc5f95f2d46a78407b28cb9d8382e41cb18244
evansuva <evans@cs.virginia.edu>
2014-01-28 20:39:52 -0500

    More edits to part 3

--- code/03/list.rs
+++ code/03/list.rs
@@ -1,22 +1,71 @@
-type List = Option<~Node>;
+#[deriving(Clone)]
+#[deriving(ToStr)]
+type LinkedList = Option<~Node>;
 
+#[deriving(Clone)]
 struct Node {
    val: int,
-   tail: List
+   tail: LinkedList
 }
 
-fn construct_list(n: int, x: int) -> List {
-    if n == 0 {
-        None
-    } else {
-        Some(~Node{val: x, tail: construct_list(n - 1, x + 1)})
+trait Length {
+   fn length(&self) -> int;
+}
+
+#[allow(dead_code)] 
+fn length(p: LinkedList) -> int {
+   match p {
+      None => { 0 }
+      Some(node) => { 1 + length(node.tail) }
+   }
+}
+
+impl Length for LinkedList {
+   fn length(&self) -> int {
+      match self {
+        &Some(ref node) => { 1 + node.tail.length() }
+        &None => 0
+      }
+   }
+}
+
+fn construct_list(n: int, x: int) -> LinkedList {
+    match n {
+        0 => { None }
+        _ => { Some(~Node{val: x, tail: construct_list(n - 1, x + 1)}) }
     }
 }
 
+fn print_list(p: LinkedList) -> ~str {
+    match p {
+        None => { ~"" }
+        Some(node) => { node.val.to_str() + ", " + print_list(node.tail) }
+    }
+}
+
+trait Map {
+   fn mapr(&mut self, fn(int) -> int);
+}
+
+
+impl Map for LinkedList {
+    fn mapr(&mut self, f: fn(int) -> int) {
+         match(*self) {
+            None => { }
+            Some(ref mut current) => { 
+               current.val = f(current.val);
+               current.tail.mapr(f); 
+            }
+        } 
+    } 
+}
+
+fn inc(n: int) -> int { n + 1 }
+fn double(n: int) -> int { n * 2 }
+
 fn main() {
-    let node1 = Node {val:10,  tail: None};
-    let mut node2 = Node {val: 10, tail: None};
-    node2.val = 15; 
-        // node1.val = 15; 
-    let l10 = construct_list(5, 10);
+    let mut l10 : LinkedList = construct_list(5, 10);
+    l10.mapr(inc);
+    l10.mapr(double);
+    println!("List: {:s}", print_list(l10.clone()));
 }
