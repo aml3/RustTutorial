3f64fda6a7b6262f3e20f736150ad01d477bcd7a
mfeckie <mfeckie@gmail.com>
2014-09-03 10:22:40 +0800

    Bug and style fix
    missing ! from println and change N to n to appease the compiler!

--- code/collatz.rs
+++ code/collatz.rs
@@ -2,7 +2,7 @@ use std::os;
 
 fn main() {
 	if os::args().len() < 2 {
-		println("Error: Please provide a number as argument.");
+		println!("Error: Please provide a number as argument.");
 		return;
 	}
 
@@ -10,10 +10,10 @@ fn main() {
 	println!("{:d} has {:d} Collatz steps", i, collatz(i));
 }
 
-fn collatz(N: int) -> int {
-	if N == 1 { return 0; }
-	match N % 2 {
-		0 => { 1 + collatz(N/2) }
-		_ => { 1 + collatz(N*3+1) }
+fn collatz(n: int) -> int {
+	if n == 1 { return 0; }
+	match n % 2 {
+		0 => { 1 + collatz(n/2) }
+		_ => { 1 + collatz(n*3+1) }
 	}
 }
